79cc0b6ca9cff5a4b922e727ebe2a4e705686646
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-06-06 13:11:02 -0400

    added comment

--- src/main/java/org/example01/App.java
+++ src/main/java/org/example01/App.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Alex Goodwin
+ */
+
 package org.example01;
 import java.util.Scanner;
 
@@ -5,7 +10,7 @@ public class App
 {
     public static void main( String[] args ) {
         Scanner name = new Scanner(System.in);
-        System.out.println("What is your name?");
+        System.out.print("What is your name?");
         String Name = name.nextLine();
         System.out.print("Hello, ");
         System.out.print(Name);
